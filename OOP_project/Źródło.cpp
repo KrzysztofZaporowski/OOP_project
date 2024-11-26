5f0112de61dd88a88d3151a8849d63d14d253b9c
Krzysztof Zaporowski <krzysztofzaporowski@gmail.com>
2024-11-26 12:16:51 +0100

    Czyta wszystko "chyba" xD

--- OOP_project/Źródło.cpp
+++ OOP_project/Źródło.cpp
@@ -10,7 +10,11 @@ int main()
 	BookDb db("tcp://localhost:3306", "root", "Krzysiu13", "library");
 	LibraryCollections collection(db.listBooks());
 	for (const auto& book : collection.getLibraryCollection()) {
-		std::cout << book.getTitle() << std::endl;
+		std::cout << book.getTitle() << ", " <<
+			book.getPublishYear() << ", " <<
+			book.getAuthorName() << " " << book.getAuthorLastname() << ", " <<
+			book.getIsbn() << ", " <<
+			book.getIsAvailable() << std::endl;
 	}
 
 	return 0;
