29ea5ca3667dc8be230247a759f8ba31604cd31d
p_ratajczak <85259929+xPompunia@users.noreply.github.com>
2025-01-14 23:15:25 +0100

    add comments

--- OOP_project/User.cpp
+++ OOP_project/User.cpp
@@ -1,11 +1,14 @@
 #include "User.h"
 
+// Class constructor
 User::User(std::string login, std::string password, std::string nickname) : login(login), password(password), nickname(nickname){}
 
+// Metothod to display basic info
 void User::displayInfo(){
 	std::cout << "Login: " << login << ", nickname: " << nickname << std::endl;
 }
 
+// Getters for login, nickname and password
 std::string User::getLogin() const{
 	return login;
 }
